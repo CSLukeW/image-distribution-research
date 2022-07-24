abac7031fbba72b3d302f5ed725915b9937afd26
Luke Williams <williams.luke.2001@gmail.com>
2022-07-24 13:52:46 -0400

    added utility functions

---
+++ src/util.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Subset
+import numpy as np
+
+
+def split_train_val(org_train_set, valid_ratio=0.1):
+
+    num_train = len(org_train_set)
+
+    split = int(np.floor(valid_ratio * num_train))        
+
+    indices = list(range(num_train))
+
+    np.random.shuffle(indices)
+
+    train_idx, val_idx = indices[split:], indices[:split]
+
+    new_train_set = Subset(org_train_set, train_idx)
+    val_set = Subset(org_train_set, val_idx)
+
+    assert num_train - split == len(new_train_set)
+    assert split == len(val_set)
+
+    return new_train_set, val_set
+
+
+def test(net, loader, device):
+    # prepare model for testing (only important for dropout, batch norm, etc.)
+    net.eval()
+    
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data, target in loader:
+
+            data, target = data.to(device), target.to(device)
+            
+            output = net(data)
+            test_loss += F.nll_loss(output, target).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += (pred.eq(target.data.view_as(pred)).sum().item())
+            
+            total = total + 1
+
+    print('Test set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss, correct, len(loader.dataset),
+        (100. * correct / len(loader.dataset))))
+    
+    return 100.0 * correct / len(loader.dataset)
+
+
+def train(net, loader, optimizer, epoch, device, log_interval=100):
+    # prepare model for training (only important for dropout, batch norm, etc.)
+    net.train()
+
+    correct = 0
+    for batch_idx, (data, target) in enumerate(loader):
+
+        data, target = data.to(device), target.to(device)
+        
+        # clear up gradients for backprop
+        optimizer.zero_grad()
+        output = F.log_softmax(net(data), dim=1)
+
+        # use NLL loss
+        loss = F.nll_loss(output, target)
+
+        # compute gradients and make updates
+        loss.backward()
+        optimizer.step()
+
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += (pred.eq(target.data.view_as(pred)).sum().item())
+
+        if batch_idx % log_interval == 0:
+            print('\nTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(loader.dataset), 100. * batch_idx / len(loader), loss.item()))
+
+    print('\tAccuracy: {:.2f}%'.format(100.0 * correct / len(loader.dataset)))    
